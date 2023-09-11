531529184546949a77fc85896d183365ae2d4c7c
Ana Fábia Coelho <afcoelho44@gmail.com>
2023-09-10 22:56:11 -0300

    Ver o three

---
+++ basic.js
@@ -0,0 +1,14 @@
+console.log(THREE);
+
+// window.onload = () => {
+//   let testeEntityAdded = false;
+
+//   const el = document.querySelector("[gps-new-camera]");
+
+//   el.addEventListener("gps-camera-update-position", (e) => {
+//     if (!testeEntityAdded) {
+//         alert(`Pegue a primeira posicao: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+//         const entity = document.createElement('')
+//     }
+//   });
+// };
