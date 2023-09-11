cdf2d047f5023f5650f4591400c47abf9dcb76a5
Ana Fábia Coelho <afcoelho44@gmail.com>
2023-09-10 23:37:21 -0300

    Retornando a-gltf-model

--- basic.js
+++ basic.js
@@ -8,8 +8,18 @@ window.onload = () => {
       alert(
         `Pegue a primeira posicao: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`
       );
-      const entity = document.querySelector("a-gltf-model ");
-      console.log(entity);
+      const entity = document.querySelector("a-entity");
+      entity.setAttribute("scale", {
+        x: 5,
+        y: 5,
+        z: 5,
+      });
+      entity.setAttribute("gps-new-entity-place", {
+        latitude: e.detail.position.latitude + 0.001,
+        longitude: e.detail.position.longitude,
+      });
+      document.querySelector("a-scene").append(entity);
     }
+    testeEntityAdded = true;
   });
 };
