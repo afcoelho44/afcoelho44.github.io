7cd211f07946e30fe5e296f20db16aea2c4d7271
Ana Fábia Coelho <afcoelho44@gmail.com>
2023-11-29 12:19:19 -0300

    Test establishments RA

--- basic.js
+++ basic.js
@@ -1,3 +1,43 @@
+const establishments = [
+  {
+    "id": 1,
+    "name": "Praça Verde",
+    "active": true,
+    "latitude": -49.67234674,
+    "longitude": -43.45678909,
+    "hour": [
+      12,
+      0
+    ],
+    "attractions": "bailão dos veios",
+    "fees_costs": "R$ 10",
+    "comments": [
+      {
+        "id": 52,
+        "title": "Interessante",
+        "content": "Não gostei, mas tem potencial",
+        "media": "https://cdn.pnghd.pics/data/1759/dancing-gif-36.gif"
+      }
+    ],
+    "tags": []
+  },
+  {
+    "id": 2,
+    "name": "Praça Azul",
+    "active": true,
+    "latitude": -49.67234674,
+    "longitude": -43.45678909,
+    "hour": [
+      12,
+      0
+    ],
+    "attractions": "bailão dos dos novos",
+    "fees_costs": "R$ 20",
+    "comments": [],
+    "tags": []
+  }
+];
+
 window.onload = () => {
   let testeEntityAdded = false;
 
@@ -8,17 +48,19 @@ window.onload = () => {
       alert(
         `Pegue a primeira posicao: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`
       );
-      const entity = document.querySelector("a-entity");
-      entity.setAttribute("scale", {
-        x: 9,
-        y: 9,
-        z: 9,
+      establishments.forEach(establishment => {
+        const entity = document.querySelector("a-entity");
+        entity.setAttribute("scale", {
+          x: 9,
+          y: 9,
+          z: 9,
+        });
+        entity.setAttribute("gps-new-entity-place", {
+          latitude: establishment.latitude,
+          longitude: establishment.longitude,
+        });
+        document.querySelector("a-scene").append(entity);
       });
-      entity.setAttribute("gps-new-entity-place", {
-        latitude: e.detail.position.latitude + 0.001,
-        longitude: e.detail.position.longitude,
-      });
-      document.querySelector("a-scene").append(entity);
     }
     testeEntityAdded = true;
   });
